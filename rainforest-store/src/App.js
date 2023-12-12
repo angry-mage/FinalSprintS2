b2dcea768b435c8aeee44e6d4ad23eff46702a3c
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2023-12-12 11:12:19 -0330

    With React Router

--- rainforest-store/src/App.js
+++ rainforest-store/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Outlet, Link} from "react-router-dom";
+import './index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <header>
+      <img src="../images/logo.png" className="headLogo" alt="Rainforest Logo" width="400px"/>
+    <nav>
+      <Link to="/" className="nav-link"><img src="../images/home.png" alt="Home" width="50px"/> </Link>
+      <Link to="/cart" className="nav-link"><img src="../images/cart.png" alt="Cart" height="67px" /></Link>
+    </nav>
+    </header>
+    <Outlet />
+</>
+);
 }
 
 export default App;
--- rainforest-store/src/index.js
+++ rainforest-store/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Products from "./pages/Products";
+import Description from "./pages/Description";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
 import reportWebVitals from './reportWebVitals';
 
+
+export default function Main() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Products />} />
+          <Route path="description" element={<Description />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Main />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
