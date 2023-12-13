0547a1db342f1249e05b28eb3839c15bcf461d01
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2023-12-13 16:54:10 -0330

    description and some comments added

--- rainforest-store/src/hooks/useFetch.jsx
+++ rainforest-store/src/hooks/useFetch.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
-//This custom hook will retrieve the API with all production listing information. It can be used multiple times to fetch the information for any page that will need specific information about the products being sold. 
+//This custom hook will retrieve the API with all production listing information. It can be used multiple times to fetch the information for any page that will need specific information about the products being sold. Id is added as a parameter to ensure we can use it to fetch each specific item on the Description page.
 
-const useFetch = (url) => {
+const useFetch = (url, id) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -10,7 +10,7 @@ const useFetch = (url) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(url, id);
         const json = await response.json();
         setData(json);
         setLoading(false);
@@ -21,7 +21,7 @@ const useFetch = (url) => {
     };
 
     fetchData();
-  }, [url]);
+  }, [url, id]);
 
   return { data, loading, error };
 };
--- rainforest-store/src/pages/Description.jsx
+++ rainforest-store/src/pages/Description.jsx
@@ -1,5 +1,30 @@
+import useFetch from "../hooks/useFetch";
+import {useParams} from "react-router-dom";
+import './Description.css'
+
+//When an item is clicked in the Products.jsx page this page will then open. useParams was imported from the React Router library to ensure that the item ID could be used to fetch the data for that particular item in the API used in the useFetch hook.  The styling for this page can be found on the Description.css page. 
 const Description = () => {
-  return <h1>Description</h1>;
-};
+  const {id} = useParams();
+  const { data, loading, error } = useFetch('https://fakestoreapi.com/products');
+
+  if (loading) {
+    return <div>Loading......</div>;
+  }
+  if (error) {
+    return <div> Error: {error.message}</div>;
+  }
+  const item = data.find((item) => item.id.toString() === id);
+
+  return (
+      
+        <div className="desBox">         
+          <span><img id="desImg" src={item.image} alt={item.title} />
+          </span>
+          <span id="des">
+          <h4>{item.title}</h4>
+          <p>{item.description}</p>
+          <p>${item.price}</p></span>
+        </div> 
+  )};
 
 export default Description;
