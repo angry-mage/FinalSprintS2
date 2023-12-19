73fa690bb1f13e27b02954891388b7dc378d1793
jayEag <jay.eagles@Keyin.com>
2023-12-19 12:19:02 -0330

    Did A LOT

--- rainforest-store/src/pages/Cart.jsx
+++ rainforest-store/src/pages/Cart.jsx
@@ -1,11 +1,39 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext'
 import Footer from '../components/Footer'
-
+// import './Cart.css'
+ 
 const Cart = () => {
-  return (
-  <>
-  <h1>Cart</h1>
+  const { cartItems, removeFromCart, total } = useContext(CartContext);
   
-  </>
-)};
-
+  return (
+    <>
+      <h1 id="cartHeader">Your Cart</h1>
+      <div className="cartItems">
+        {cartItems.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id}>
+               <img src={item.image} alt={item.name} width="50px" height="50px" />
+               <span> {item.name}</span>
+               <span> {item.price}</span>
+               <button onClick={() => removeFromCart(item.id)}>
+                  Remove
+               </button>
+               </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      
+      <div>Total: {total} </div>
+  
+      <Footer />
+    </>
+  );
+ 
+};
+ 
 export default Cart;
